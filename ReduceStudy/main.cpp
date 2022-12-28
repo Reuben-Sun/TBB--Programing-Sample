75d0662dc9e28cf6091e36f2a9615e86144251f8
Reuben <2824016820@qq.com>
2022-12-28 10:49:07 +0800

    reduce

--- ReduceStudy/main.cpp
+++ ReduceStudy/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <tbb/tbb.h>
 
+//求最大值
 int pmax(const std::vector<int> &arr){
     int max_value = tbb::parallel_reduce(
             tbb::blocked_range<int>(0, arr.size()),
@@ -19,8 +20,30 @@ int pmax(const std::vector<int> &arr){
     return max_value;
 }
 
+//求pi
+double calcPI(int degree){
+    double dx = 1.0 / degree;
+    double sum = tbb::parallel_reduce(
+            tbb::blocked_range<int>(0, degree),
+            0.0,
+            [=](const tbb::blocked_range<int> &r, double init) -> double{
+                for(int i = r.begin(); i != r.end(); ++i){
+                    double x = (i + 0.5)*dx;
+                    double h = std::sqrt(1 - x*x);  //勾股定理
+                    init += h * dx;
+                }
+                return init;
+            },
+            [](double x, double y) -> double{
+                return x+y;
+            }
+        );
+    return 4 * sum;
+}
+
 int main(){
     std::vector<int> a = {1,4,5,8,9,3,4,6,0};
-    std::cout << pmax(a);
+    std::cout << pmax(a) << std::endl;
+    std::cout << "PI = " << calcPI(100000) << std::endl;
     return 0;
 }
